5f7791baf75032048cf10dca86d6550b8bc88958
Zal <50516841+rizgustiadi@users.noreply.github.com>
2020-12-16 16:03:21 +0700

    Update Akeno.py

--- userbot/modules/Akeno.py
+++ userbot/modules/Akeno.py
@@ -4,12 +4,12 @@ from userbot import CMD_HELP
 from userbot.events import register
 
 
-@register(outgoing=True, pattern="^.Akeno$")
+@register(outgoing=True, pattern="^.akeno$")
 async def shalom(e):
-    await e.edit(f"Klik[`Ini](https://github.com/rizgustiadi/AkenoXNew)`AkenoXNew Github Page")
+    await e.edit(f"Klik`[Ini](https://github.com/rizgustiadi/AkenoXNew)`AkenoXNew Github Page")
     CMD_HELP.update(
         {
-            "Akeno": ".Akeno\
+            "Akeno": ".akeno\
 \nUsage: gives a nice Gitub Page as output."
         }
     )
