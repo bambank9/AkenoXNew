a7284645612fbf6b9cba01be80059b7ec3627027
Zal <50516841+rizgustiadi@users.noreply.github.com>
2020-12-17 06:59:16 +0700

    Update Akeno.py

--- userbot/modules/Akeno.py
+++ userbot/modules/Akeno.py
@@ -7,7 +7,7 @@ from userbot.events import register
 @register(outgoing=True, pattern="^.akeno$")
 async def shalom(e):
     await e.edit(f"Klik [`ini](https://github.com/rizgustiadi/AkenoXNew)`AkenoXNew Github Page")
-    await (f"[`Github Profile](https://github.com/rizgustiadi)`")
+    await e(f"[`Github Profile](https://github.com/rizgustiadi)`")
     CMD_HELP.update(
         {
             "Akeno": ".akeno\
