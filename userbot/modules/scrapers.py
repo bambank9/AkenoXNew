d744fa8234badad90dd4c99dad1f704a5f78154c
Zal <50516841+rizgustiadi@users.noreply.github.com>
2020-12-17 18:30:09 +0700

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -349,17 +349,19 @@ async def _(event):
     if event.fwd_from:
         return
     if "trim" in event.raw_text:
-        # https://t.me/c/1220993104/192075
         return
+    x = await eor(event, "Translating...")
     input_str = event.pattern_match.group(1)
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
         text = previous_message.message
-        lan = input_str or "en"
+        lan = input_str or "ml"
     elif "|" in input_str:
         lan, text = input_str.split("|")
     else:
-        await event.edit("`.tr LanguageCode` as reply to a message")
+        await x.edit(
+            f"`{CMD_HNDLR}tr LanguageCode` as reply to a message.\nLanguage codes can be found [here](https://t.me/TeleBotHelpChat/22678)",
+        )
         return
     text = emoji.demojize(text.strip())
     lan = lan.strip()
@@ -367,17 +369,17 @@ async def _(event):
     try:
         translated = translator.translate(text, dest=lan)
         after_tr_text = translated.text
-        # TODO: emojify the :
-        # either here, or before translation
-        output_str = """**TRANSLATED** from {} to {}
-{}""".format(
-            translated.src,
-            lan,
-            after_tr_text
+        output_str = """
+**Tʀᴀɴsʟᴀᴛɪᴏɴ**
+
+**{} ➟ {}**
+
+`{}`""".format(
+            translated.src, lan, after_tr_text
         )
-        await event.edit(output_str)
+        await x.edit(output_str)
     except Exception as exc:
-        await event.edit(str(exc))
+        await x.edit(f"Error\n `{str(exc)}`")
 
 
 
