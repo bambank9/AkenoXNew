e9e58c2572cc4e4cb03b0af252809445310d0dbc
Zal <50516841+rizgustiadi@users.noreply.github.com>
2020-12-18 05:52:28 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -241,7 +241,7 @@ async def amireallyalive(alive):
         f"➥ ✘ `BotVersion             :`v{VER}\n"
         f"➥ ✘ `Telethon version       :v{version.__version__} `\n"
         f"➥ ✘ `Python Version         :v{python_version()} `\n"
-        f"➥ ✘ `Owner                  :{DEAFULTUSER}`\n"
+        f"➥ ✘ `Owner                  :{DEFAULTUSER}`\n"
         f"➥ ✘ `Load modules           :`{len(modules)} \n"
         f"➥ ✘[`🇮🇩Github Repository🇮🇩](https://github.com/rizgustiadi/AkenoXNew)`\n"
     )
