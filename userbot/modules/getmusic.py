553b46c7261a4fc5dd5b8e7d2a9a2859d2cdeb68
Zal <50516841+rizgustiadi@users.noreply.github.com>
2021-03-30 10:57:43 +0700

    Update getmusic.py

--- userbot/modules/getmusic.py
+++ userbot/modules/getmusic.py
@@ -68,11 +68,44 @@ async def _(event):
         await bot.forward_messages(event.chat_id, respond.message)
         await bot.send_read_acknowledge(event.chat_id)
 
+@register(outgoing=True, pattern="^.nts (?: |$)(.*)")
+async def_(event):
+    if event.fwd_from:
+        return
+    song = netase.pattren_match.group(1)
+    chat = "@WooMaiBot"
+    link = f"/netease {song}"
+    await netase.edit("```Getting Your Music```")    
+    async with bot.conversation(chat) as conv:
+        await asyncio.sleep(2)
+        await netase.edit("`Downloading.... Please Wait`")
+        try:
+            msg = await conv.send_message(link)
+            response = await conv.get_response()
+            respond = await conv.get_response()
+            """ - don't spam notif - """
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await netase.reply("```Please unblock @WooMaiBot and try again```")
+            return
+        await netase.edit("`Sending Your Music`")
+        await asyncio.sleep(3)
+        await bot.send_file(netase.chat_id, respond)
+    msg = await netase.client.delete_messages(
+        conv.chat_id [msg.id, response.id, respond.id]
+    )
+    await msg.edit(
+        f"Song name - __{link}__\nUploaded by [AkenoXNew]"
+    )
+    await netase.delete()
+        
 
 CMD_HELP.update(
     {
         "song": ">`.song` **atrist title**"
         "\nUsage: Finding and uploading song.\n"
+        "nts": ">`.nts` **Penyanyi - judul lagu**"
+        "\nUsage: Download music with @WooMaiBot\n"
         ">`.smd` **<song tittle>**"
         "\nUsage: **Download music from spotify**"
     }
