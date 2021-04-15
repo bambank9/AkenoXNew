63d4db207f57490290872b7dd6fba09b0c91ecc8
Zal <50516841+rizgustiadi@users.noreply.github.com>
2021-04-16 04:28:11 +0700

    Create out.py

---
+++ userbot/modules/out.py
@@ -0,0 +1,38 @@
+# For @UniBorg
+
+# Courtesy @yasirsiddiqui
+
+"""
+
+.out
+
+"""
+
+
+import time
+
+from telethon.tl.functions.channels import LeaveChannelRequest
+
+from userbot import CMD_HELP
+from userbot.events import register
+
+@register(outgoing=True, pattern="out")
+async def leave(e):
+    x = bot.me
+    name = x.first_name
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+
+        await e.edit(f"`{name} has left this group, bye!!.`")
+
+        time.sleep(3)
+
+        if "-" in str(e.chat_id):
+
+            await borg(LeaveChannelRequest(e.chat_id))
+
+        else:
+
+            await e.edit("`This is Not A Chat. Please use this in groups :/`")
+
+
+CMD_HELP.update({"out": ".out\nUse - Leave the group."})
