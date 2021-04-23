c015381de957d8c20346aa24a801789de284d613
Zal <50516841+rizgustiadi@users.noreply.github.com>
2021-04-23 11:39:17 +0700

    Update womai.py

--- userbot/modules/womai.py
+++ userbot/modules/womai.py
@@ -7,35 +7,45 @@ from userbot import CMD_HELP, bot
 from userbot.events import register
 
 @register(outgoing=True, pattern=r"^.nts (.*)")
-async def WooMai(netase):
-    if netase.fwd_from:
+async def _(event):
+    if event.fwd_from:
         return
-    song = netase.pattern_match.group(1)
+    if event.pattern_match.group(1) == "now":
+        playing = User(LASTFM_USERNAME, lastfm).get_now_playing()
+        if playing is None:
+            return await event.edit("`Error: No current scrobble found.`")
+        artist = playing.get_artist()
+        song = playing.get_title()
+    else:
+        artist = event.pattern_match.group(2)
+        song = event.pattern_match.group(3)
+    track = str(artist) + " - " + str(song)
     chat = "@WooMaiBot"
-    link = f"/netease {song}"
-    await netase.edit("```Getting Your Music```")
-    async with bot.conversation(chat) as conv:
-        await asyncio.sleep(2)
-        await netase.edit("`Downloading... Please wait`")
-        try:
-            msg = await conv.send_message(link)
-            response = await conv.get_response()
-            respond = await conv.get_response()
-            """ - don't spam notif - """
-            await bot.send_read_acknowledge(conv.chat_id)
-        except YouBlockedUserError:
-            await netase.reply("```Please unblock @WooMaiBot and try again```")
-            return
-        await netase.edit("`Sending Your Music...`")
-        await asyncio.sleep(3)
-        await bot.send_file(netase.chat_id, respond)
-    msg = await netase.client.delete_messages(
-        conv.chat_id, [msg.id, response.id, respond.id]
-    )
-    await msg.edit(
-        f"Song name - __{link}__\nUploaded by [TeleBot](https://t.me/TeleBotSupport)"
-    )
-    await netase.delete()
+    link = f"/netease {track}"
+    await event.edit("`Searching...`")
+    try:
+        async with bot.conversation(chat) as conv:
+            await asyncio.sleep(2)
+            await event.edit("`Processing... Please wait`")
+            try:
+                msg = await conv.send_message(link)
+                response = await conv.get_response()
+                respond = await conv.get_response()
+                await bot.send_read_acknowledge(conv.chat_id)
+            except YouBlockedUserError:
+                await event.reply("`Please unblock @WooMaiBot and try again`")
+                return
+            await event.edit("`Sending Your Music...`")
+            await asyncio.sleep(3)
+            await bot.send_file(event.chat_id, respond)
+        await event.client.delete_messages(
+            conv.chat_id, [msg.id, response.id, respond.id]
+        )
+        await event.delete()
+    except TimeoutError:
+        return await event.edit(
+            "`Error: `@WooMaiBot` is not responding or Song not found!.`"
+        )
 
 CMD_HELP.update(
         {
