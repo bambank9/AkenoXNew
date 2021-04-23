fcf0de7980498261ccc16b7c1f0d30d32a2a218b
Zal <50516841+rizgustiadi@users.noreply.github.com>
2021-04-23 10:29:38 +0700

    Create dezzload.py

---
+++ userbot/modules/dezzload.py
@@ -0,0 +1,50 @@
+
+# Originally from Bothub
+# (c) 2020 TeleBot
+
+import asyncio
+import os
+
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telebot import CMD_HELP, bot
+from userbot.events import register
+
+@register(outgoing=True, pattern=r"^.dzd (.*)")
+async def DeezLoader(Deezlod):
+    if Deezlod.fwd_from:
+        return
+    d_link = Deezlod.pattern_match.group(1)
+    if ".com" not in d_link:
+        await Deezlod.edit("` I need a link to download something pro.`**(._.)**")
+    else:
+        await Deezlod.edit("**Initiating Download!**")
+    chat = "@DeezLoadBot"
+    async with bot.conversation(chat) as conv:
+        try:
+            msg_start = await conv.send_message("/start")
+            response = await conv.get_response()
+            r = await conv.get_response()
+            msg = await conv.send_message(d_link)
+            details = await conv.get_response()
+            song = await conv.get_response()
+            """ - don't spam notif - """
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await Deezlod.edit("**Error:** `unblock` @DeezLoadBot `and retry!`")
+            return
+        await bot.send_file(Deezlod.chat_id, song, caption=details.text)
+        msg = await Deezlod.client.delete_messages(
+            conv.chat_id, [msg_start.id, response.id, r.id, msg.id, details.id, song.id]
+        )
+        await msg.edit(
+            f"Song name - __{d_link}__\nUploaded by [TeleBot](https://t.me/TeleBotSupport)"
+        )
+        await Deezlod.delete()
+
+CMD_HELP.update(
+        {
+            "dzd": ".dzd\
+\nUsage: gives a nice Gitub Page as output."
+        }
+    )
