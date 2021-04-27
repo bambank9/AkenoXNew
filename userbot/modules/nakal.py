f620ce72c876d017ae4a2e5c6e56cdd0ada6a2a5
Zal <50516841+rizgustiadi@users.noreply.github.com>
2021-04-27 15:50:53 +0700

    Create nakal.py

---
+++ userbot/modules/nakal.py
@@ -0,0 +1,35 @@
+import asyncio
+from userbot CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern=r"^.comli (.*)")
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.1
+
+    animation_ttl = range(0, 101)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "fuk":
+
+        await event.edit(input_str)
+
+        animation_chars = ["👉       ✊️", "👉     ✊️", "👉  ✊️", "👉✊️💦"]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 4])
+CMD_HELP.update(
+        {
+            "nakal": ".comli\
+\nUsage: gives a nice Gitub Page as output."
+        }
+    )
