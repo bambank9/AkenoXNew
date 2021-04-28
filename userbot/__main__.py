55690c6cbb1befb7b11b4069173857fd5205adc7
Zal <50516841+rizgustiadi@users.noreply.github.com>
2021-04-28 12:30:33 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,41 +1,81 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-""" Userbot start point """
-
-from importlib import import_module
+import glob
+from telebot import bot
 from sys import argv
+from telethon import TelegramClient
+from telebot.telebotConfig import Var
+from telebot.utils import load_module, start_mybot, load_pmbot
+from pathlib import Path
+import telethon.utils
+from telebot import CMD_HNDLR
 
-from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
-from userbot import LOGS, bot, AkenoXNew_VER, CODENAME
-from userbot.modules import ALL_MODULES
+TELE = Var.PRIVATE_GROUP_ID
+BOTNAME = Var.TG_BOT_USER_NAME_BF_HER
+LOAD_MYBOT = Var.LOAD_MYBOT
 
-VER = str(AkenoXNew_VER)
 
-INVALID_PH = (
-    "\nERROR: The Phone No. entered is INVALID"
-    "\n Tip: Use Country Code along with number."
-    "\n or check your phone number and try again !"
-)
+async def add_bot(bot_token):
+    await bot.start(bot_token)
+    bot.me = await bot.get_me()
+    bot.uid = telethon.utils.get_peer_id(bot.me)
 
-try:
-    bot.start()
-except PhoneNumberInvalidError:
-    print(INVALID_PH)
-    exit(1)
 
-for nama_modul in ALL_MODULES:
-    imported_module = import_module("userbot.modules." + nama_modul)
+async def startup_log_all_done():
+    try:
+        await bot.send_message(TELE, f"**AkenoXNew has been deployed.\nSend** `{CMD_HNDLR}alive` **to see if the bot is working.\n\nAdd** @{BOTNAME} **to this group and make it admin for enabling all the features of TeleBot**")
+    except BaseException:
+        print("Either PRIVATE_GROUP_ID is wrong or you have left the group.")
 
-LOGS.info(f"sedang berjalan Akeno {CODENAME} v{VER}.\n")
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot = None
+    if Var.TG_BOT_USER_NAME_BF_HER is not None:
+        print("Initiating Inline Bot")
+        # ForTheGreatrerGood of beautification
+        bot.tgbot = TelegramClient(
+            "TG_BOT_TOKEN",
+            api_id=Var.APP_ID,
+            api_hash=Var.API_HASH
+        ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
+        print("Initialisation finished, no errors")
+        print("Starting Userbot")
+        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        print("Startup Completed")
+    else:
+        bot.start()
 
-LOGS.info(
-    "Selamat, AkenoXNew sudah berhasil dijalankan\n"
-    "Untuk Test AkenoXNew Ketik .on or .live\n"
-    "Jangan Lupa Untuk Update Ketik .update! Have fun!"
-)
+path = 'userbot/modules/*.py'
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_module(shortname.replace(".py", ""))
+
+print("AkenoXNew has been deployed! ")
+
+print("Setting up TGBot")
+path = "userbot/modules/mybot/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        start_mybot(shortname.replace(".py", ""))
+
+if LOAD_MYBOT == "True":
+    path = "userbot/modules/mybot/pmbot/*.py"
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as f:
+            path1 = Path(f.name)
+            shortname = path1.stem
+            load_pmbot(shortname.replace(".py", ""))
+    print("TGBot set up completely!")
+
+print("TGBot set up - Level - Basic")
+print("AkenoXNew has been fully deployed! Do Visit @OreBaka")
+bot.loop.run_until_complete(startup_log_all_done())
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
