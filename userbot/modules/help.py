20cabeac13540b38e09b9d4c6a3e5e5f466141e1
Zal <50516841+rizgustiadi@users.noreply.github.com>
2021-04-29 16:10:24 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -35,8 +35,8 @@ async def help(event):
             string += "`" + str(i)
             string += "`\t|  "
         await event.edit("**🌪🌪🌪🌪🌪🌪**\n\n"
-                         f"**◑» Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◑» Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
-                         "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
+                         f"**OWNER : {DEFAULTUSER}**\n**◑» Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
+                         "**MODULES:**\n"
                          f"╰►| {string} ◄─\n\n")
         await event.reply(f"\n**🔥 Contoh** : **Ketik** `.help afk` **Untuk Informasi Pengunaan.\nAtau Bisa Juga Ketik** `.helpme` **Untuk Main Menu Yang Lain-Nya.** 🔥")
         await asyncio.sleep(1000)
