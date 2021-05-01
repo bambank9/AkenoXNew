be477272a7766a1e317af6ca10b93155eab4aca3
Zal <50516841+rizgustiadi@users.noreply.github.com>
2021-05-01 12:33:22 +0700

    Update __init__.py

--- userbot/modules/sql_helper/__init__.py
+++ userbot/modules/sql_helper/__init__.py
@@ -1,24 +1,25 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
+from userbot import DB_URI
 
-
-# the secret configuration specific things
-from userbot.akenoConfig import Var
+BASE = declarative_base()
 
 
 def start() -> scoped_session:
-    engine = create_engine(Var.DB_URI)
+    engine = create_engine(DB_URI)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
-try:
-    BASE = declarative_base()
-    SESSION = start()
-except AttributeError as e:
-    # this is a dirty way for the work-around required for #23
-    print("DB_URI is not configured. Features depending on the database might have issues.")
-    print(str(e))
+def delete_table(table_name):
+    metadata = MetaData()
+    metadata.reflect(engine)
+    table = metadata.tables.get(table_name)
+    if table is not None:
+        LOGS.info(f"Deleting '{table_name}' table...")
+        BASE.metadata.drop_all(engine, [table], checkfirst=True)
+
+
+SESSION = start()
